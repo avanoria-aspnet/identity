f2087a2c4580d47bdd6e31a61c8740c9f9c9c710
Hans Mattin-Lassei - Avanoria AB <hans.mattin-lassei@avanoria.se>
2026-03-18 14:38:41 +0100

    auth form and layout added

---
+++ Presentation.WebApp/Controllers/AuthenticationController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Presentation.WebApp.Models.Authentication.Register;
+
+namespace Presentation.WebApp.Controllers;
+
+public class AuthenticationController : Controller
+{
+
+    [HttpGet("sign-up")]
+    public IActionResult SignUp()
+    {
+        return View();
+    }
+
+    [HttpPost("sign-up")]
+    public IActionResult SignUp(RegisterEmailForm form)
+    {
+        return View();
+    }
+}
---
+++ Presentation.WebApp/Models/Authentication/Register/RegisterEmailForm.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.WebApp.Models.Authentication.Register;
+
+public class RegisterEmailForm
+{
+    [Required(ErrorMessage = "Email address is required")]
+    [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Invalid format for email address")]
+    [Display(Name = "Email Address", Prompt = "name@example.com")]
+    public string Email { get; set; } = null!;
+}
